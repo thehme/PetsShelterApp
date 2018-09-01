ac8d9abb06a29745fa9ee2404189b2c521ff18a8
brendadev <brenda@koneksahealth.com>
2018-09-01 12:14:54 -0400

    replaced db.rawQuery with Cursor query method and created a projection for this query

--- app/src/main/java/com/example/android/pets/CatalogActivity.java
+++ app/src/main/java/com/example/android/pets/CatalogActivity.java
@@ -60,9 +60,10 @@ public class CatalogActivity extends AppCompatActivity {
         // Create and/or open a database to read from it
         db = mDbHelper.getReadableDatabase();
 
-        // Perform this raw SQL query "SELECT * FROM pets"
-        // to get a Cursor that contains all rows from the pets table.
-        Cursor cursor = db.rawQuery("SELECT * FROM " + PetEntry.TABLE_NAME, null);
+        String[] projection = { PetEntry._ID };
+
+        Cursor cursor = db.query(PetEntry.TABLE_NAME, projection, null, null, null, null, null);
+
         try {
             // Display the number of rows in the Cursor (which reflects the number of rows in the
             // pets table in the database).
