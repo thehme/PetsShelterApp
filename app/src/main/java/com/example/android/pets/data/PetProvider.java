28d93497467cd13c0b75a01a6ff422884724cc42
brendadev <brenda@koneksahealth.com>
2018-09-08 15:14:53 -0400

    added getType method definition by defining MIME types

--- app/src/main/java/com/example/android/pets/data/PetProvider.java
+++ app/src/main/java/com/example/android/pets/data/PetProvider.java
@@ -231,6 +231,14 @@ public class PetProvider extends ContentProvider {
      */
     @Override
     public String getType(Uri uri) {
-        return null;
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case PETS:
+                return PetContract.CONTENT_LIST_TYPE;
+            case PET_ID:
+                return PetContract.CONTENT_ITEM_TYPE;
+            default:
+                throw new IllegalStateException("Unknown URI " + uri + " with match " + match);
+        }
     }
 }
