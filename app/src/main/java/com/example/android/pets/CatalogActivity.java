f058ffe875c6d9699179c8f6af671605e9d98040
brendadev <brenda@koneksahealth.com>
2018-09-08 11:13:07 -0400

    replaced the direct cursor db query with a getContentResolver query

--- app/src/main/java/com/example/android/pets/CatalogActivity.java
+++ app/src/main/java/com/example/android/pets/CatalogActivity.java
@@ -4,6 +4,7 @@ import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -57,9 +58,6 @@ public class CatalogActivity extends AppCompatActivity {
         // and pass the context, which is the current activity.
         mDbHelper = new PetsDbHelper(this);
 
-        // Create and/or open a database to read from it
-        db = mDbHelper.getReadableDatabase();
-
         String[] projection = {
                 PetEntry._ID,
                 PetEntry.COLUMN_PET_NAME,
@@ -68,7 +66,13 @@ public class CatalogActivity extends AppCompatActivity {
                 PetEntry.COLUMN_PET_WEIGHT
         };
 
-        Cursor cursor = db.query(PetEntry.TABLE_NAME, projection, null, null, null, null, null);
+        Cursor cursor = getContentResolver().query(
+                PetEntry.CONTENT_URI, // to get entire pets table
+                projection,
+                null,
+                null,
+                null
+        );
 
         try {
             // Display the number of rows in the Cursor (which reflects the number of rows in the
@@ -99,6 +103,7 @@ public class CatalogActivity extends AppCompatActivity {
                         + currentGender + " - "
                         + currentWeight);
             }
+
         } finally {
             // Always close the cursor when you're done reading from it. This releases all its
             // resources and makes it invalid.
